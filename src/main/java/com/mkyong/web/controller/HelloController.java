7a0fdcb68988dd652acf68f26be39aa2acba0c78
ybmadhu <ybmadhu707@gmail.com>
2017-04-06 00:36:50 +0530

    cahnged

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -13,8 +13,8 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "hello";
+		model.addAttribute("message", "Spring 3 MVC Hello World My example");
+		return " Darning hello";
 
 	}
 
@@ -22,7 +22,7 @@ public class HelloController {
 	public ModelAndView hello(@PathVariable("name") String name) {
 
 		ModelAndView model = new ModelAndView();
-		model.setViewName("hello Darling");
+		model.setViewName("hello");
 		model.addObject("msg", name);
 
 		return model;
